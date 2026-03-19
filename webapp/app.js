97a28cb56cde484a6aded92a291f1a95c9280ef7
Alisa <127328313+AlisaV365@users.noreply.github.com>
2026-03-19 11:24:46 +0300

    Update app.js

--- webapp/app.js
+++ webapp/app.js
@@ -3,39 +3,35 @@ tg.expand();
 
 const user = tg.initDataUnsafe.user;
 
-if (!user) {
-    document.body.innerHTML = "❌ Ошибка пользователя";
-} else {
-    const userId = user.id;
+const API = "https://robloxhomework-production.up.railway.app";
 
-    fetch("https://robloxhomework-production.up.railway.app/tasks/" + userId)
-        .then(res => res.json())
-        .then(tasks => {
-            const container = document.getElementById("tasks");
+const userId = user.id;
 
-            if (tasks.length === 0) {
-                container.innerHTML = "😴 Пока нет заданий";
-                return;
-            }
+fetch(API + "/tasks/" + userId)
+    .then(res => res.json())
+    .then(tasks => {
+        const container = document.getElementById("tasks");
 
-            tasks.forEach(task => {
-                const div = document.createElement("div");
-                div.innerHTML = `
-                    <div style="
-                        background:#222;
-                        padding:10px;
-                        margin:10px;
-                        border-radius:10px;
-                    ">
-                        🧹 ${task[1]} <br>
-                        💎 ${task[2]} монет
-                    </div>
-                `;
-                container.appendChild(div);
-            });
-        })
-        .catch(err => {
-            document.body.innerHTML = "❌ Ошибка загрузки";
-            console.error(err);
+        tasks.forEach(task => {
+            const div = document.createElement("div");
+
+            div.innerHTML = `
+                <div style="background:#222;padding:10px;margin:10px;border-radius:10px">
+                    🧹 ${task[1]} <br>
+                    💎 ${task[2]} <br>
+                    📊 ${task[3]} <br>
+                    <button onclick="complete(${task[0]})">✅ Выполнено</button>
+                </div>
+            `;
+
+            container.appendChild(div);
+        });
+    });
+
+function complete(taskId) {
+    fetch(API + "/complete/" + taskId, {method: "POST"})
+        .then(() => {
+            alert("Отправлено на проверку 👌");
+            location.reload();
         });
 }
