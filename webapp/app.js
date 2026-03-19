85a7b5aec2e0fdf23f34d1884d70e15f79676cd4
Alisa <127328313+AlisaV365@users.noreply.github.com>
2026-03-19 14:42:26 +0300

    Update app.js

--- webapp/app.js
+++ webapp/app.js
@@ -3,35 +3,41 @@ tg.expand();
 
 const user = tg.initDataUnsafe.user;
 
-const API = "https://robloxhomework-production.up.railway.app";
+const container = document.getElementById("tasks");
 
-const userId = user.id;
+if (!user) {
+    container.innerHTML = "❌ Нет user (открой через Telegram)";
+} else {
+    const userId = user.id;
 
-fetch(API + "/tasks/" + userId)
-    .then(res => res.json())
-    .then(tasks => {
-        const container = document.getElementById("tasks");
+    container.innerHTML = "⏳ Загружаем задания...";
 
-        tasks.forEach(task => {
-            const div = document.createElement("div");
+    fetch("https://robloxhomework-production.up.railway.app/tasks/" + userId)
+        .then(res => res.json())
+        .then(tasks => {
 
-            div.innerHTML = `
-                <div style="background:#222;padding:10px;margin:10px;border-radius:10px">
-                    🧹 ${task[1]} <br>
-                    💎 ${task[2]} <br>
-                    📊 ${task[3]} <br>
-                    <button onclick="complete(${task[0]})">✅ Выполнено</button>
-                </div>
-            `;
+            if (!tasks.length) {
+                container.innerHTML = "😴 Нет заданий";
+                return;
+            }
 
-            container.appendChild(div);
-        });
-    });
+            container.innerHTML = "";
 
-function complete(taskId) {
-    fetch(API + "/complete/" + taskId, {method: "POST"})
-        .then(() => {
-            alert("Отправлено на проверку 👌");
-            location.reload();
+            tasks.forEach(task => {
+                const div = document.createElement("div");
+
+                div.innerHTML = `
+                    <div style="background:#222;color:white;padding:10px;margin:10px;border-radius:10px">
+                        🧹 ${task[1]} <br>
+                        💎 ${task[2]}
+                    </div>
+                `;
+
+                container.appendChild(div);
+            });
+        })
+        .catch(err => {
+            container.innerHTML = "❌ Ошибка загрузки";
+            console.error(err);
         });
 }
