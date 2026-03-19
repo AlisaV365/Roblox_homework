03b8bb6d0eb5a1eac37986ca36d6efa748dac459
Alisa <127328313+AlisaV365@users.noreply.github.com>
2026-03-19 07:54:56 +0300

    Update app.js

--- webapp/app.js
+++ webapp/app.js
@@ -1,4 +1,20 @@
 const tg = window.Telegram.WebApp;
 tg.expand();
 
-fetch("https://robloxhomework-production.up.railway.app/tasks/" + userId)
+const user = tg.initDataUnsafe.user;
+
+if (!user) {
+    document.body.innerHTML = "❌ Ошибка: нет пользователя";
+} else {
+    const userId = user.id;
+
+    fetch("https://robloxhomework-production.up.railway.app/tasks/" + userId)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(err => {
+            document.body.innerHTML = "❌ Ошибка загрузки";
+            console.error(err);
+        });
+}
