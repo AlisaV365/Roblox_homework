36ff2d8ad03ad44b0426bfe0c821b80fb7a45c0e
Alisa <127328313+AlisaV365@users.noreply.github.com>
2026-03-19 10:36:33 +0300

    Update app.js

--- webapp/app.js
+++ webapp/app.js
@@ -4,14 +4,35 @@ tg.expand();
 const user = tg.initDataUnsafe.user;
 
 if (!user) {
-    document.body.innerHTML = "❌ Ошибка: нет пользователя";
+    document.body.innerHTML = "❌ Ошибка пользователя";
 } else {
     const userId = user.id;
 
     fetch("https://robloxhomework-production.up.railway.app/tasks/" + userId)
         .then(res => res.json())
-        .then(data => {
-            console.log(data);
+        .then(tasks => {
+            const container = document.getElementById("tasks");
+
+            if (tasks.length === 0) {
+                container.innerHTML = "😴 Пока нет заданий";
+                return;
+            }
+
+            tasks.forEach(task => {
+                const div = document.createElement("div");
+                div.innerHTML = `
+                    <div style="
+                        background:#222;
+                        padding:10px;
+                        margin:10px;
+                        border-radius:10px;
+                    ">
+                        🧹 ${task[1]} <br>
+                        💎 ${task[2]} монет
+                    </div>
+                `;
+                container.appendChild(div);
+            });
         })
         .catch(err => {
             document.body.innerHTML = "❌ Ошибка загрузки";
